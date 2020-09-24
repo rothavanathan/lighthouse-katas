1e628f0703786bdb27d845d12f8875716fcb777d
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-09-24 01:10:44 +0000

    finished square-code.js

--- square-code.js
+++ square-code.js
@@ -5,9 +5,21 @@ const squareCode = function(message) {
   let runOnMessage = message.split(" ").join("");
   let i = 0;
   let columns = Math.ceil(Math.sqrt(runOnMessage.length));
-  console.log(columns)
-  let rows = Math.floor(runOnMessage.length / columns);
-  //figure out the matrix here
+  let rows = Math.ceil(Math.sqrt(runOnMessage.length));
+  //for each row
+  for (let r = 0; r < rows ; r++){
+    //we know each row starts with the equal indexed letter
+    newMessage += runOnMessage[r]
+    //then we want to add the character columns to initial character index n number of times...
+    for (let c = 1; c < columns; c++){
+      //... if it's within the message length
+      if (((r+1) + (c * rows)) <= runOnMessage.length){
+        newMessage += runOnMessage[(r + (c * rows))]
+      }
+    }
+    //add newline before next row
+    newMessage += " "
+  }
   return newMessage;
 };
 
